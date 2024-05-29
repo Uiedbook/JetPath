d38ffd465ef7bf64227dc6b088c145331822ad25
fridaycandour <fridaymichaels662@gmail.com>
2024-05-29 22:42:22 +0100

    tested deno

--- tests/plug.js
+++ tests/plug.js
@@ -1,51 +1,56 @@
-import { createReadStream, createWriteStream, unlink, } from "fs";
+import { createReadStream, createWriteStream, unlink, } from "node:fs";
 import os from "node:os";
 import { cwd } from "node:process";
 import busboy from "busboy";
 import { JetPlugin } from "../dist/index.js";
-import path from "path";
+import path from "node:path";
 export const busboyjet = new JetPlugin({
     name: "busboyjet",
     version: "1.0.0",
     executor({ runtime, server }) {
         return {
             formData(ctx) {
-                return new Promise((res) => {
+                return new Promise((res, rej) => {
                     const data = {};
-                    const bb = busboy({ headers: ctx.request.headers });
-                    bb.on("file", (name, file, info) => {
-                        const oldPath = path.join(os.tmpdir(), info.filename);
-                        // info.location = saveTo;
-                        info.saveTo = (name) => {
-                            const newPath = path.resolve(cwd(), name);
-                            return new Promise((res, rej) => {
-                                var readStream = createReadStream(oldPath);
-                                var writeStream = createWriteStream(newPath);
-                                readStream.on("error", (e) => {
-                                    rej(e);
-                                });
-                                writeStream.on("error", (e) => {
-                                    rej(e);
-                                });
-                                readStream.on("close", function () {
-                                    unlink(oldPath, (e) => {
+                    try {
+                        const bb = busboy({ headers: ctx.request.headers });
+                        bb.on("file", (name, file, info) => {
+                            const oldPath = path.join(os.tmpdir(), info.filename);
+                            info.location = oldPath;
+                            info.saveTo = (name) => {
+                                const newPath = path.resolve(cwd(), name);
+                                return new Promise((res, rej) => {
+                                    var readStream = createReadStream(oldPath);
+                                    var writeStream = createWriteStream(newPath);
+                                    readStream.on("error", (e) => {
                                         rej(e);
                                     });
-                                    res(undefined);
+                                    writeStream.on("error", (e) => {
+                                        rej(e);
+                                    });
+                                    readStream.on("close", function () {
+                                        unlink(oldPath, (e) => {
+                                            rej(e);
+                                        });
+                                        res(undefined);
+                                    });
+                                    readStream.pipe(writeStream);
                                 });
-                                readStream.pipe(writeStream);
-                            });
-                        };
-                        data[name] = info;
-                        file.pipe(createWriteStream(oldPath));
-                    });
-                    bb.on("field", (name, val) => {
-                        data[name] = val !== "undefined" ? val : undefined;
-                    });
-                    bb.on("close", () => {
-                        res(data);
-                    });
-                    ctx.request.pipe(bb);
+                            };
+                            data[name] = info;
+                            file.pipe(createWriteStream(oldPath));
+                        });
+                        bb.on("field", (name, val) => {
+                            data[name] = val !== "undefined" ? val : undefined;
+                        });
+                        bb.on("close", () => {
+                            res(data);
+                        });
+                        ctx.request.pipe(bb);
+                    }
+                    catch (error) {
+                        rej(error);
+                    }
                 });
             },
         };
--- tests/plug.ts
+++ tests/plug.ts
@@ -4,12 +4,12 @@ import {
   createReadStream,
   createWriteStream,
   unlink,
-} from "fs";
+} from "node:fs";
 import os from "node:os";
 import { cwd } from "node:process";
 import busboy from "busboy";
 import { AppCTX, JetPlugin } from "../dist/index.js";
-import path from "path";
+import path from "node:path";
 
 export const busboyjet = new JetPlugin({
   name: "busboyjet",
@@ -17,42 +17,46 @@ export const busboyjet = new JetPlugin({
   executor({ runtime, server }) {
     return {
       formData(ctx: AppCTX) {
-        return new Promise((res) => {
+        return new Promise((res, rej) => {
           const data = {};
-          const bb = busboy({ headers: ctx.request.headers });
-          bb.on("file", (name: string, file: any, info: any) => {
-            const oldPath = path.join(os.tmpdir(), info.filename);
-            // info.location = saveTo;
-            info.saveTo = (name: string) => {
-              const newPath = path.resolve(cwd(), name);
-              return new Promise((res, rej) => {
-                var readStream = createReadStream(oldPath);
-                var writeStream = createWriteStream(newPath);
-                readStream.on("error", (e) => {
-                  rej(e);
-                });
-                writeStream.on("error", (e) => {
-                  rej(e);
-                });
-                readStream.on("close", function () {
-                  unlink(oldPath, (e) => {
+          try {
+            const bb = busboy({ headers: ctx.request.headers });
+            bb.on("file", (name: string, file: any, info: any) => {
+              const oldPath = path.join(os.tmpdir(), info.filename);
+              info.location = oldPath;
+              info.saveTo = (name: string) => {
+                const newPath = path.resolve(cwd(), name);
+                return new Promise((res, rej) => {
+                  var readStream = createReadStream(oldPath);
+                  var writeStream = createWriteStream(newPath);
+                  readStream.on("error", (e) => {
                     rej(e);
                   });
-                  res(undefined);
+                  writeStream.on("error", (e) => {
+                    rej(e);
+                  });
+                  readStream.on("close", function () {
+                    unlink(oldPath, (e) => {
+                      rej(e);
+                    });
+                    res(undefined);
+                  });
+                  readStream.pipe(writeStream);
                 });
-                readStream.pipe(writeStream);
-              });
-            };
-            data[name] = info;
-            file.pipe(createWriteStream(oldPath));
-          });
-          bb.on("field", (name: string, val: any) => {
-            data[name] = val !== "undefined" ? val : undefined;
-          });
-          bb.on("close", () => {
-            res(data);
-          });
-          ctx.request.pipe(bb);
+              };
+              data[name] = info;
+              file.pipe(createWriteStream(oldPath));
+            });
+            bb.on("field", (name: string, val: any) => {
+              data[name] = val !== "undefined" ? val : undefined;
+            });
+            bb.on("close", () => {
+              res(data);
+            });
+            ctx.request.pipe(bb);
+          } catch (error) {
+            rej(error);
+          }
         });
       },
     };
